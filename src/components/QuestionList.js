9e6ec1ffca1127704eea1b7bcb97d26044418a77
Eliza Cohen <elizacohen5@gmail.com>
2024-05-21 10:11:36 -0400

    complete react-hooks-fetch-crud-lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((questionData) => setQuestions(questionData));
+  }, []);
+
+  function handleDeleteQuestion(deletedQuestion) {
+    const updatedQuestions = questions.filter(
+      (question) => question.id !== deletedQuestion.id
+    );
+    setQuestions(updatedQuestions);
+  }
+
+  function handleUpdateQuestion(updatedQuestion) {
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === updatedQuestion.id) {
+        return updatedQuestion;
+      } else {
+        return question
+      }
+    })
+    setQuestions(updatedQuestions)
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul className="Questions">
+        {questions.map((question) => (
+          <QuestionItem
+            question={question}
+            key={question.id}
+            handleDeleteQuestion={handleDeleteQuestion}
+            handleUpdateQuestion={handleUpdateQuestion}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
